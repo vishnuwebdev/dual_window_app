0bce457158b95c7163f111670ed891ebd9b38542
DWI <dwivg10021@gmail.com>
2026-07-24 14:01:19 +0530

    Update the sms template static config

--- lib/core/registration/settings_sync_service.dart
+++ lib/core/registration/settings_sync_service.dart
@@ -170,7 +170,7 @@ class SettingsSyncService {
     final body = jsonEncode({
       'config': config,
       'cvmaster_config': cvmasterConfig,
-      'sms_template': 'Hello ', //cfg.smsTemplate,
+      'sms_template': cfg.smsTemplate,
       'lockers_sizes':
           cfg.lockerMapping.map((e) => e.size.toUpperCase()).toList(),
       'db_entries':
